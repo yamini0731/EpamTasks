6064924ad6b71530428cd26ce4682de396f18bcc
SALVERU YAMINI <60654641+yamini0731@users.noreply.github.com>
2020-07-23 22:01:54 +0530

    Create Operations.java

---
+++ design-principles/Operations.java
@@ -0,0 +1,15 @@
+package operations;
+public class Operations {
+    public float add(float a,float b){
+        return a+b;
+    }
+    public float subtract(float a,float b){
+        return a-b;
+    }
+    public float multiply(float a,float b){
+        return a*b;
+    }
+    public float divide(float a,float b){
+        return a/b;
+    }
+}
